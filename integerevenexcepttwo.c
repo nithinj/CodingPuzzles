ab893fb7566efeccc9f68020eaa7e4d13eef3d81
nithinj <nithin.johnson@altair.com>
2016-11-06 14:49:32 +0530

    Initializing new project

---
+++ integerevenexcepttwo.c
@@ -0,0 +1,7 @@
+/*
+ * You are given an integer array, where all numbers except for TWO numbers appear even number of times. 
+ *
+ * Q: Find out the two numbers which appear odd number of times.
+ */
+
+

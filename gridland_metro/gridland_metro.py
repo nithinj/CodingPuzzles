e82d6729dbeeef18a3c3eeab44991e26d8035460
nithinj <nithin.johnson@altair.com>
2018-07-04 16:55:20 +0530

    final submission

--- gridland_metro/gridland_metro.py
+++ gridland_metro/gridland_metro.py
@@ -9,13 +9,19 @@ for i in range(k):
     inp = stdin.readline().split()
     inp = [int(x) for x in inp]
     if inp[0] in mat:
-        if (inp[1] < mat[inp[0]][0]):
-            mat[inp[0]][0] = inp[1]
-        if (inp[2] > mat[inp[0]][1]):
-            mat[inp[0]][1] = inp[2]
+        for item in mat[inp[0]]:
+            if (((inp[1] >= item[0]) and (inp[1] <= item[1])) or
+                    ((inp[2] >= item[0]) and (inp[2] <= item[1]))):
+                if (inp[1] < item[0]):
+                    item[0] = inp[1]
+                if (inp[2] > item[1]):
+                    item[1] = inp[2]
+            else:
+                mat[inp[0]].append(inp[1:])
     else:
-        mat[inp[0]] = inp[1:]
+        mat[inp[0]] = [inp[1:]]
 tot = 0
 for val in mat.itervalues():
-    tot += val[1] - val[0] + 1
+    for item in val:
+        tot += item[1] - item[0] + 1
 print(str((n * m) - tot))

eb58903bfdf253adde9f88d6366369e8ea3f3364
nithinj <nithin.johnson@altair.com>
2018-07-04 16:05:10 +0530

    using dict instead of matrix

--- gridland_metro/gridland_metro.py
+++ gridland_metro/gridland_metro.py
@@ -4,17 +4,18 @@ from sys import stdin
 inp = stdin.readline().split()
 inp = [int(x) for x in inp]
 (n, m, k) = (inp[0], inp[1], inp[2])
-mat = []
-for i in range(n):
-    mat.append([1] * m)
+mat = {}
 for i in range(k):
     inp = stdin.readline().split()
     inp = [int(x) for x in inp]
-    for j in range(inp[1] - 1, inp[2]):
-        mat[inp[0] - 1][j] = 0
-#print("Matrix:")
-#print(str(mat))
+    if inp[0] in mat:
+        if (inp[1] < mat[inp[0]][0]):
+            mat[inp[0]][0] = inp[1]
+        if (inp[2] > mat[inp[0]][1]):
+            mat[inp[0]][1] = inp[2]
+    else:
+        mat[inp[0]] = inp[1:]
 tot = 0
-for i in range(n):
-    tot += sum(mat[i])
-print(str(tot))
+for val in mat.itervalues():
+    tot += val[1] - val[0] + 1
+print(str((n * m) - tot))

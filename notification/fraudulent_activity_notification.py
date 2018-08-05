45e2f1db87512d936282756eacaec28d321d05f8
nithinj <nithinpottackal@gmail.com>
2018-08-05 19:14:53 +0530

    Improving performance

--- notification/fraudulent_activity_notification.py
+++ notification/fraudulent_activity_notification.py
@@ -6,6 +6,8 @@ import random
 import re
 import sys
 
+DEBUG = 0
+
 
 def activityNotifications(expenditure, d):
     notification = 0
@@ -17,27 +19,33 @@ def activityNotifications(expenditure, d):
             exp_list.sort()
             sorted_ = 1
         else:
-            exp_list.remove(expenditure[i - d])
             next_ = expenditure[i - 1]
-            (low, high) = (0, len(exp_list))
-            while(low <= high):
-                mid = int((low + high) / 2)
-                elm = exp_list[mid]
-                if (mid == 0 and elm > next_) or (mid == len(exp_list) - 1 and elm < next_):
-                    exp_list.insert(mid, next_)
-                    break
-                elif (next_ >= exp_list[mid - 1] and next_ <= exp_list[mid]):
-                    exp_list.insert(mid, next_)
-                    break
-                if next_ > elm:
-                    low = mid
-                else:
-                    high = mid
+            removal = expenditure[i - d - 1]
+            if (next_ != removal):
+                exp_list.remove(expenditure[i - d - 1])
+                (low, high) = (0, len(exp_list))
+                while(low <= high):
+                    mid = int((low + high) / 2)
+                    elm = exp_list[mid]
+                    if (mid == 0 and elm > next_):
+                        exp_list.insert(mid, next_)
+                        break
+                    elif (mid == len(exp_list) - 1 and elm < next_):
+                        exp_list.insert(mid + 1, next_)
+                        break
+                    elif (next_ >= exp_list[mid - 1] and next_ <= exp_list[mid]):
+                        exp_list.insert(mid, next_)
+                        break
+                    if next_ > elm:
+                        low = mid + 1
+                    else:
+                        high = mid - 1
 
         if (d % 2 == 0):
-            med = exp_list[int(d / 2)] + exp_list[int(d / 2) - 1]
+            med = (exp_list[int(d / 2)] + exp_list[int(d / 2) - 1]) / 2.0
         else:
             med = exp_list[int(d / 2)]
+
         if expenditure[i] >= med * 2:
             notification += 1
 

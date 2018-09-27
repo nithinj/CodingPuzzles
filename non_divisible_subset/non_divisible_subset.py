5e20fc92ee74d922d6b617bc519271d661b1af86
nithinj <nithin.johnson@altair.com>
2018-09-27 12:24:20 +0530

    maps of remainders

--- non_divisible_subset/non_divisible_subset.py
+++ non_divisible_subset/non_divisible_subset.py
@@ -1,50 +1,32 @@
 #!/bin/python3
 
-DBG = 1
+DBG = 0
 
 
 def nonDivisibleSubset(k, S):
-    list_of_sets = []
-    for i in range(len(S)):
-        for j in range(i + 1, len(S)):
-            found = 0
-            if DBG:
-                print("S[i]: " + str(S[i]) + " S[j]: " + str(S[j]))
-            if ((S[i] + S[j]) % k != 0):
-                for elm in list_of_sets:
-                    if (S[i] in elm):
-                        found = 1
-                        candidate = 1
-                        for num in (elm - {S[i]}):
-                            if ((num + S[j]) % k) == 0:
-                                candidate = 0
-                                break
-                        if candidate:
-                            if DBG:
-                                print(str(elm) + ", adding:" + str(S[j]))
-                            elm.add(S[j])
-                    if (S[j] in elm):
-                        found = 1
-                        candidate = 1
-                        for num in (elm - {S[j]}):
-                            if ((num + S[i]) % k) == 0:
-                                candidate = 0
-                                break
-                        if candidate:
-                            if DBG:
-                                print(str(elm) + ", adding:" + str(S[i]))
-                            elm.add(S[i])
-                if not found:
-                    if ((S[i] + S[j]) % k) != 0:
-                        list_of_sets.append(set([S[i], S[j]]))
-                    else:
-                        list_of_sets.append({S[i]})
-                        list_of_sets.append({S[j]})
-
+    remainders = [0] * k
+    S = list(set(S))
     if DBG:
-        max(list_of_sets, key=lambda arg: len(arg))
-    return len(max(list_of_sets, key=lambda arg: len(arg)))
-
+        print(str(S))
+    for num in S:
+        remainders[int(num % k)] += 1
+    if DBG:
+        print(str(remainders))
+    total = 0
+    i = 0
+    j = len(remainders)
+    while i <= j:
+        if i == 0 or (i == j):
+            if remainders[i]:
+                total += 1
+        else:
+            if (remainders[i] > remainders[j]):
+                total += remainders[i]
+            else:
+                total += remainders[j]
+        i += 1
+        j -= 1
+    return total
 
 nk = input().split()
 

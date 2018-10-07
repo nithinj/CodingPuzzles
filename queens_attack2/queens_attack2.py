6293136388b647c13bf655b463eae753c4f28912
nithinj <nithinpottackal@gmail.com>
2018-10-07 12:58:32 +0530

    final sol

--- queens_attack2/queens_attack2.py
+++ queens_attack2/queens_attack2.py
@@ -1,79 +1,63 @@
-def left(row, col):
-    global mat
-
-    if (row < 0) or (row * n + col in mat):
-        return 0
-    return 1 + left(row - 1, col)
-
-
-def right(row, col):
-    global mat
-
-    if (row > n - 1) or (row * n + col in mat):
-        return 0
-    return 1 + right(row + 1, col)
-
-
-def top(row, col):
-    global mat
-
-    if (col > n - 1) or (row * n + col in mat):
-        return 0
-    return 1 + top(row, col + 1)
-
-
-def bottom(row, col):
-    global mat
-
-    if (col < 0) or (row * n + col in mat):
-        return 0
-    return 1 + bottom(row, col - 1)
-
-
-def topleft(row, col):
-    global mat
-
-    if (row < 0) or (col > n - 1) or (row * n + col in mat):
-        return 0
-    return 1 + topleft(row - 1, col + 1)
-
-
-def bottomleft(row, col):
-    global mat
-
-    if (row < 0) or (col < 0) or (row * n + col in mat):
-        return 0
-    return 1 + bottomleft(row - 1, col - 1)
-
-
-def topright(row, col):
-    global mat
-
-    if (row > n - 1) or (col > n - 1) or (row * n + col in mat):
-        return 0
-    return 1 + topright(row + 1, col + 1)
-
-
-def bottomright(row, col):
-    global mat
-
-    if (row > n - 1) or (col < 0) or (row * n + col in mat):
-        return 0
-    return 1 + bottomright(row + 1, col - 1)
-
 
 def queens_attack():
     global mat, queen
     count = 0
 
-    count += left(queen[0] - 1, queen[1])
-    count += right(queen[0] + 1, queen[1])
-    count += top(queen[0], queen[1] + 1)
-    count += bottom(queen[0], queen[1] - 1)
-    count += topright(queen[0] + 1, queen[1] + 1)
-    count += topleft(queen[0] - 1, queen[1] + 1)
-    count += bottomleft(queen[0] - 1, queen[1] - 1)
-    count += bottomright(queen[0] + 1, queen[1] - 1)
+    [row, col] = queen
+    row -= 1
+    while (row >= 0) and (row * n + col not in mat):
+        count += 1
+        row -= 1
+
+    [row, col] = queen
+    row += 1
+    while (row < n) and (row * n + col not in mat):
+        count += 1
+        row += 1
+
+    [row, col] = queen
+    col += 1
+    while (col < n) and (row * n + col not in mat):
+        count += 1
+        col += 1
+
+    [row, col] = queen
+    col -= 1
+    while (col >= 0) and (row * n + col not in mat):
+        count += 1
+        col -= 1
+
+    [row, col] = queen
+    row -= 1
+    col += 1
+    while (row >= 0) and (col < n) and (row * n + col not in mat):
+        count += 1
+        row -= 1
+        col += 1
+
+    [row, col] = queen
+    row -= 1
+    col -= 1
+    while (row >= 0) and (col >= 0) and (row * n + col not in mat):
+        count += 1
+        row -= 1
+        col -= 1
+
+    [row, col] = queen
+    row += 1
+    col += 1
+    while (row < n) and (col < n) and (row * n + col not in mat):
+        count += 1
+        row += 1
+        col += 1
+
+    [row, col] = queen
+    row += 1
+    col -= 1
+    while (row < n) and (col >= 0) and (row * n + col not in mat):
+        count += 1
+        row += 1
+        col -= 1
 
     return count
 
